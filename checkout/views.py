c840795ecef5a51abfd9ecfe7c77ba8f548bc2b7
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2020-08-01 14:02:35 +0000

    update in signals, added 'checkout_success' template

--- checkout/views.py
+++ checkout/views.py
@@ -81,9 +81,9 @@ def checkout(request):
     if not stripe_public_key:
         messages.warning(request, 'Strip public key is missing. Did you forget to set it in your enviroment?')
 
-        order_form = OrderForm()
-        template = 'checkout/checkout.html'
-        context = {
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
             'order_form': order_form,
             'stripe_public_key': stripe_public_key,
             'client_secret': intent.client_secret,
