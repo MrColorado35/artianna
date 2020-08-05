e873aa3a38f468fdf4825af28a1e547300a59da8
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2020-08-05 00:13:00 +0000

    Added order creation from webhook

--- checkout/urls.py
+++ checkout/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('checkout_seccess/<order_number>', views.checkout_success, name='checkout_success'),
     path('wh/', webhook, name='webhook'),
     path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
-    
 ]
