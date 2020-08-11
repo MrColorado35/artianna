5544381b930f642071485729ac7ad62524b54b72
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2020-08-11 20:40:34 +0000

    Changed secret key and set debug

--- artianna/settings.py
+++ artianna/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'eez+$%5&--gco360sdtbd05fwrob^udnzj2#(n2q!1@c%o+!^='
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['artianna.herokuapp.com', 'localhost', 'artianna.com']
 
