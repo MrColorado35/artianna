953ce86ec5213b898676625a204879d2f9d64ac6
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2020-08-12 19:24:15 +0000

    Test: first try if github-heroku-s3 connection is fixed

--- artianna/settings.py
+++ artianna/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
 
     # Other
     'crispy_forms',
-    'storages',
+    'storages', 
 ]
 
 MIDDLEWARE = [
@@ -172,11 +172,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 if 'USE_AWS' in os.environ:
     # S3 Bucket Config vars
